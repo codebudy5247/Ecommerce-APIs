b3146d7e09ec63f0619156c39ad8eb2ab85d30b1
codebudy5247 <a.shekhar5247@gmail.com>
2020-12-17 22:34:56 +0530

    fetching products from backend

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,28 @@
-const express = require('express')
-const products = require('./data/products')
+const express = require("express");
+const products = require("./data/products");
 
-const app = express()
-
+const app = express();
 
 //bodyparser
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/api/products/:id', (request, response) => {
-    const productId = Number(request.params.id);
-    const getProduct = products.find((p) => p._id === productId);
-  
-    if (!getProduct) {
-      response.status(500).send('Product not found.')
-    } else {
-      response.json(getProduct);
-    }
-  });
+app.get("/api/products/:id", (request, response) => {
+  const productId = Number(request.params.id);
+  const getProduct = products.find((p) => p._id === productId);
 
-app.listen(5000, console.log('Server running on port 5000'))
+  if (!getProduct) {
+    response.status(500).send("Product not found.");
+  } else {
+    response.json(getProduct);
+  }
+});
+
+app.listen(5000, console.log("Server running on port 5000"));
