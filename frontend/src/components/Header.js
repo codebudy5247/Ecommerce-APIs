03b7ab22fb79a1767692814b339f302a73e152b9
codebudy5247 <a.shekhar5247@gmail.com>
2020-12-17 20:16:10 +0530

    serve products from backend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,15 +14,15 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-            <LinkContainer to="/cart">
-              <Nav.Link>
-                <i className="fas fa-shopping-cart"></i>Cart
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
-              <Nav.Link>
-                <i className="fas fa-user"></i>Sign In
-              </Nav.Link>
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Sign In
+                </Nav.Link>
               </LinkContainer>
             </Nav>
           </Navbar.Collapse>
@@ -31,5 +31,7 @@ const Header = () => {
     </header>
   );
 };
+
+//Todo:Implement burger Menu
 
 export default Header;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 
-const ProductScreen = () => {
-    return (
-        <div>
-           Product 
-        </div>
-    )
+const ProductScreen = ()=>{
+    return <div>Product</div>
 }
-
-export default ProductScreen
+export default ProductScreen;
