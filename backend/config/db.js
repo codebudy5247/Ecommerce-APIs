7bb3db94699439720b275e336d12d483d9d01479
codebudy5247 <a.shekhar5247@gmail.com>
2021-07-20 13:35:29 +0530

    :hammer:

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI_DEV, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
